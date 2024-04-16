7e93ceef93db57de91001101f4fa0b527eb1951d
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-04-16 11:43:58 -0400

    connecting to database and creating signinup function

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,21 +1,48 @@
-import { Image, ScrollView, Text, View } from 'react-native'
+import { Alert, Image, ScrollView, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { getCurrentUser, signIn } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const SignIn = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext()
   const [form, setForm] = useState({
     email: '',
     password: ''
   })
 
-  const [isSumbitting, setIsSumbitting] = useState(false)
+  const [isSumbitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {}
+  const submit = async () => {
+    if(form.email === "" || form.password === "" ) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+
+    setIsSumbitting(true)
+    
+    try {
+      await signIn(
+        form.email,
+        form.password,
+      )
+
+      const result = await getCurrentUser()
+      setUser(result)
+      setIsLoggedIn(true)
+
+      Alert.alert("Success", "User signed in successfully");
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
 
   return (
     <SafeAreaView className="bg-primary h-full">
